072ae1d5216cb0fbff5408e1bc69b1e0e63307aa
Vinícius Vieira Modesto <vinicius.modesto611@gmail.com>
2025-04-22 12:14:38 -0300

    Classes Criadas

---
+++ Acelera2025/Acelera2025/Class/PeopleManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acelera2025.Class
+{
+    internal class PeopleManager
+    {
+        public static PeopleManager Instance { get; set; }
+
+        private List<Person> peopleDB = new List<Person>();
+        public Person myself;
+
+        public PeopleManager() {
+            Instance = new PeopleManager();
+        }
+
+        public List<Person> GetPeople()
+        {
+            return peopleDB;
+        }
+
+        public void AddPerson(Person person)
+        {
+            peopleDB.Add(person);
+        }
+
+        public void RemovePerson(Person person) {
+            int index = peopleDB.IndexOf(person);
+            
+        }
+    }
+}
---
+++ Acelera2025/Acelera2025/Class/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acelera2025.Class
+{
+    internal class Person
+    {
+        private string name;
+        private string cpf;
+        private string email;
+        private string password;
+        private Date birthday;
+
+        public Person(string name, string cpf, string email, string password, Date birthday) { 
+            this.name = name;
+            this.cpf = cpf;
+            this.email = email;
+            this.password = password;
+            this.birthday = birthday;
+        }
+    }
+}
