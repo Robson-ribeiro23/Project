604d6945e53f2314ba1113c076821258dfe9d5e4
Robson-ribeiro23 <robsonindalecio23@gmail.com>
2025-05-12 23:34:15 -0300

    classe empresa
    mudanças na classe pessoa e criação da classe empresa

---
+++ Acelera2025/Acelera2025/Class/Empresa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acelera2025.Class
+{
+    class Empresa
+    {
+    }
+}
